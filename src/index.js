3991ba2de2832262677bfd240bc53346591c52cd
annashipilo <a.a.shipilo@gmail.com>
2018-03-14 00:00:15 +0300

    longest sequence

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+  var obj = new Object();
+  var max = 1;
+  var count = 1;
+
+  for (var i = 0; i < array.length; i++) {
+    obj[array[i]] = array[i];
+  }
+
+  var arr_keys = Object.keys(obj);
+
+  if(arr_keys.length === 0){
+    return 0;
+  }
+
+  for (var i = 0; i < arr_keys.length; i++) {
+    arr_keys[i] = parseInt(arr_keys[i], 10);
+  }
+
+  for (var j = 0; j < arr_keys.length; j++) {
+    if (arr_keys[j] + 1 === arr_keys[j + 1]) {
+      count++;
+      if (count > max) {
+        max = count;
+      }
+    } else {
+      count = 1;
+    }
+  }
+  return max;
 }
